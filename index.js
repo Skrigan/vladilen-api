5003f0b753dddd3d77c902084b377c3c8904d282
Skrigan <sk_dm-821@mail.ru>
2023-02-03 02:08:01 +0300

    fix4

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
+const PORT =  process.env.PORT || 3000;
 const app = express();
 
-const port = process.env.PORT || 5002;
+// app.use(express.static(path.resolve(__dirname, 'static'))); // - Хранение всей статики на серваке, чтоб сервак её понимал
 
-app.use(express.json());
-app.get("/api/", (req, res) => {
-	return 'hw';
+app.get('/', (req, res) => {
+	// res.send('<h2>Api is running!</h2>');
+	res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 })
 
-app.listen(port, ()=> {
-	console.log(`Server has been started on port ${port}...`);
+app.get('/api/server', (req, res) => {
+	res.json({test: 42});
 })
+
+app.listen(PORT, () => {
+	console.log(`Server has been started on port ${PORT}...`)
+});
