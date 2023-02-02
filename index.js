0dbe34013f5df05c4d91607b84fd4c3dfcaa0524
Skrigan <sk_dm-821@mail.ru>
2023-02-03 01:58:00 +0300

    fix: add use

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path';
 const __dirname = path.resolve();
 const PORT = 3000;
 const app = express();
-
+app.use(express.json());
 // app.use(express.static(path.resolve(__dirname, 'static'))); // - Хранение всей статики на серваке, чтоб сервак её понимал
 
 // app.get('/', (req, res) => {
@@ -12,9 +12,9 @@ const app = express();
 	// res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 // })
 
-app.get('/api/server', (req, res) => {
-	res.json({test: 42});
-})
+// app.get('/api/server', (req, res) => {
+// 	res.json({test: 42});
+// })
 
 app.listen(PORT, () => {
 	console.log(`Server has been started on port ${PORT}...`)
