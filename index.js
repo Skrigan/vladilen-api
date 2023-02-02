fe06ed064f91695d6d894adf8f1acff366712960
Skrigan <sk_dm-821@mail.ru>
2023-02-03 01:51:32 +0300

    fix: remove get

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express();
 
 // app.use(express.static(path.resolve(__dirname, 'static'))); // - Хранение всей статики на серваке, чтоб сервак её понимал
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 	// res.send('<h2>Api is running!</h2>');
 	// res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
-})
+// })
 
 app.get('/api/server', (req, res) => {
 	res.json({test: 42});
