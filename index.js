945036411b3075bd82e82073c9652d9dbe4a85a1
Skrigan <sk_dm-821@mail.ru>
2023-02-03 01:39:57 +0300

    feat: add simple server

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
+const PORT = 3000;
+const app = express();
+
+// app.use(express.static(path.resolve(__dirname, 'static'))); // - Хранение всей статики на серваке, чтоб сервак её понимал
+
+app.get('/', (req, res) => {
+	// res.send('<h2>Api is running!</h2>');
+	res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+})
+
+app.get('/api/server', (req, res) => {
+	res.json({test: 42});
+})
+
+app.listen(PORT, () => {
+	console.log(`Server has been started on port ${PORT}...`)
+});
